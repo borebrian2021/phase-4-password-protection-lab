336650c46e8135b64ef9f4234379a4684fba5d73
borebrian2021 <bkimutai2021@gmail.com>
2022-10-10 00:22:00 +0300

    commit tree a3585aa41ab057c5e5137e2ba5659c7cf3f6c0dc

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+
+    def create
+        user =User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id]= user.id
+            render json: user ,status: :ok
+        else
+            render json: {error:{login: "invalid username and passsword"}},status: :unauthorized    
+          end
+       
+    end
+
+    def destroy
+        session.delete(:user_id)
+        head :no_content
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+resources :user, only:[:create,:delete,:update]
+# get '/login' , to: 'users#create'
+post '/signup',to: 'users#create'
+get '/me',to: 'users#show'
+delete '/logout',to: 'users#destroy'
+post '/login', to: 'users#create'
 end
